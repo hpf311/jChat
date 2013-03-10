bf0019fa87bfc55d20cba27a6297adc9695b010a
Thomas Traxler <thomas.traxler@student.tgm.ac.at>
2013-03-10 20:24:47 +0100

    P2p Arbeit begonnen

--- P2pCom.java
+++ P2pCom.java
@@ -1,21 +1,229 @@
 ﻿package jChat;
 
+import java.awt.Color;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Properties;
 
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Listener;
 
-public class P2pCom implements JChatCom
-{
+/**
+ * P2P Logik
+ * 
+ * @author Thomas Traxler
+ * @version 2013-02-22
+ * 
+ */
+// TODO: Kommando verwaltung nur Serverseitig Client-Objekte mit weiteren Daten
+// ausstatten
+public class P2pCom extends Thread implements JChatCom {
+	private JChatAuthenticator jca;
+	private JChatGUI jcg;
+	private String peerName;
+	private Color c;
+	private Hashtable peers;
+	private boolean overhead;
+
+	public P2pCom(P2pAuth jca, JChatGUI jcg) {
+
+		this.jca = jca;
+		// try {
+		// this.sleep(1000);
+		// } catch (InterruptedException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		this.jcg = jcg;
+		int[] cc = { 0, 0, 0 };
+		jcg.AddChatListener(new SrvChatListener());
+		jcg.addMessage("Peer gestartet", cc);
+		this.start();
+		jcg.open();
+	}
+
+	
+	public void run() {
+		String message = "";
+		while (true) {
+			try {
+				if (jca.ready(message)) {
+					if (overhead) {// Wenn es nicht User Kommunikation ist
+						if (message.equals("end")) {// Overhead ende
+							overhead = false;
+						} else if (true) {// Nicht User Kommunikation
+						}
+					} else {
+						if (message != "") {
+							message = message + " ";
+							String[] splitM = message.split(" ", 2);
+							if (splitM[0].equals("/msg")) {
+								int[] a = { c.getRed(), c.getGreen(),c.getBlue() };
+								jcg.addMessage(peerName + ":" + message, a);// fuegt eingehende Nachricht lokal hinzu
+								sendMessage(peerName + ":" + message + "\n");// leitet eingehende Nachricht an alle Clients weiter.
+							} else {
+								if (splitM[0].equals("/error")){
+									System.out.println("Error: " + splitM[1]);
+								}else if (splitM[0].equals("/overhead")) {
+									overhead = true;
+								}
+							}
+						}
+					}
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Server ChatListener Leitet ausgehende nachrichten an sendMessage weiter
+	 * und fuegt sie der lokalen Anzeige hinzu Leitet Beenden ein.
+	 * 
+	 * @author Thomas Traxler
+	 * 
+	 */
+	public class SrvChatListener implements ChatListener {
+
+		// public void actionPerformed(ActionEvent e) {
+		// String message = jcg.equalsChatLine(e.getSource());
+		// if ( !message.equals("")) {
+		// sendMessage("SERVER: "+message);
+		// jcg.addMessage("SERVER: "+message, null);
+		// }
+		// if ( jcg.equalsDisconnect(e.getSource()))
+		// stopConnection();
+		// }
+
+		@Override
+		public void handleEvent(Event e) {
+			System.out.println("A");
+			String message = "";
+			if (e.character == '\b') {
+
+			} else {
+
+				message = jcg.equalsChatLine(e.widget);
+				if (!message.equals("")) {
+					if(message.charAt(0)=='/'){
+						
+					}else{
+						sendMessage("SERVER: " + message + "\n");
+						jcg.addMessage("SERVER: " + message, null);
+					}
+				}
+			}
+
+		}
+	}
+
+	/**
+	 * Listenerthread für einen Clientinput
+	 * 
+	 * @author Thomas Traxler
+	 * 
+	 */
+	// public class P2pList extends Properties implements APeer{
+	// private String peerName;
+	// private Color c = new Color (0,0,0);
+	//
+	// public P2pList(String name){
+	// this.peerName = name;
+	// }
+	//
+	// public void updated (PrintWriter pw){
+	// this.list(pw);
+	// }
+	//
+	// public void update (BufferedReader br) throws IOException{
+	// this.load(br);
+	// }
+	//
+	//
+	// // public void addSocket( Socket s) throws IOException{
+	// // this.s=s;
+	// // this.br = new BufferedReader(new
+	// InputStreamReader(s.getInputStream()));
+	// // this.pw = new PrintWriter(s.getOutputStream(), true);
+	// //
+	// // pw.print("/name "+peerName+"\n");
+	// // }
+	//
+	// // public void run(){
+	// // while(true){
+	// // try {
+	// // this.sleep(100);
+	// // } catch (InterruptedException e1) {
+	// // e1.printStackTrace();
+	// // }
+	// // try {
+	// // if(br.ready()){
+	// // String message =br.readLine() ;
+	// // if(message != ""){
+	// // if(message.charAt(0)!='/'){
+	// // int[] a = {c.getRed(),c.getGreen(),c.getBlue()};
+	// // jcg.addMessage(peerName+":"+message, a );//fuegt eingehende Nachricht
+	// lokal hinzu
+	// // sendMessage(peerName+":"+message+"\n");//leitet eingehende Nachricht
+	// an alle Clients weiter.
+	// // }else{
+	// // message = message+" ";
+	// // String [] splitM = message.split(" ",2);
+	// // if (splitM[0].equals("/error"))
+	// // System.out.println("Error: "+splitM[1]);
+	// // else if (splitM[0].equals("/name")){
+	// // if(nl.changeName(peerName, splitM[1])){
+	// // peerName = splitM[1];
+	// // pw.print("/name "+splitM[1]+"\n");
+	// // pw.flush();
+	// // jcg.setNameList(nl);
+	// // sendMessage("/namelist ");
+	// // ArrayList n = nl.getNames();
+	// // sendMessage(""+n.get(0));
+	// // for (int i = 1;i<nl.getNames().size();i++){
+	// // sendMessage(";"+n.get(i));
+	// // }
+	// // sendMessage("\n");
+	// // }
+	// // }
+	// // else if(splitM[0].equals("/col")){
+	// //// c = InReader.getColor(splitM[1]);
+	// // pw.print(message+"\n");
+	// // }
+	// // }
+	// // }
+	// // }
+	// // } catch (IOException e) {
+	// // e.printStackTrace();
+	// // }
+	// // }
+	// // }
+	//
+	// public String getPeerName() {
+	// return peerName;
+	// }
+	//
+	// public void setPeerName(String name) {
+	// this.peerName = name;
+	// }
+	//
+	// }
 
 	@Override
 	public void sendMessage(String message) {
-		// TODO Auto-generated method stub
-		
+
+		jca.sendMessage(message);
+
 	}
 
 	@Override
 	public void stopConnection() {
-		// TODO Auto-generated method stub
-		
-	}
+		jca.stopConnection();
 
+	}
 
 }
