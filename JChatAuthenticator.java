ff63d2dfe49f7ad825677a5c415cc8ed87ae81e6
Nanak Tattyrek <nanak@tattyrek.at>
2013-03-10 21:18:08 +0100

    Wii'd like to play

--- JChatAuthenticator.java
+++ JChatAuthenticator.java
@@ -16,6 +16,7 @@ public interface JChatAuthenticator
     public void sendMessage (String message);
     public void stopConnection();
     public void accept( APeer p) throws IOException;
+    public boolean ready(String msg) throws IOException;
 
 
 }
--- P2pAuth.java
+++ P2pAuth.java
@@ -6,6 +6,11 @@ import java.io.IOException;
 
 public class P2pAuth implements JChatAuthenticator
 {
+	P2pSocket p2ps;
+	
+	public P2pAuth(P2pSocket p2ps){
+		this.p2ps = p2ps;
+	}
 
 	@Override
 	public void sendMessage(String message) {
@@ -23,6 +28,10 @@ public class P2pAuth implements JChatAuthenticator
 	public void accept(APeer p) throws IOException {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public boolean ready(String msg) throws IOException{
+		return p2ps.ready(msg);
 	}
 
 
